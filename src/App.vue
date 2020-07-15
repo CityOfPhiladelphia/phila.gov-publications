300a62daff34856db0f7d78f3b26666e0a7b3e9e
m-atia <mohamed.atia@phila.gov>
2020-07-15 15:42:56 -0400

    publications work

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   
+    <Publications />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Publications from './components/Publications.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Publications
   }
 }
 </script>
