6cd2fedaff79bfd64fae3bd7b80e081e710692b6
m-atia <mohamed.atia@phila.gov>
2022-12-08 10:52:51 -0500

    fix filter label and departments selector overflow, and mobile clear button style

--- src/components/Publications.vue
+++ src/components/Publications.vue
@@ -32,7 +32,7 @@
       id="filter-results"
       class="bg-ghost-gray pam"
     >
-      <h6>Filter results</h6>
+      <h4>Filter results</h4>
       <div class="filter-container grid-x grid-margin-x">
         <div class="cell medium-5 small-10">
           <datepicker
@@ -74,7 +74,7 @@
         </div>
         <div class="cell medium-5 ">
           <a
-            class="button content-type-featured full"
+            class="button content-type-featured"
             @click="clearAllFilters()"
           >Clear filters</a>
         </div>
@@ -410,7 +410,7 @@ table {
   font-size: 14px;
 }
 #filter-results {
-  h6 {
+  h4 {
    text-align:left;
 
   }
@@ -451,6 +451,30 @@ table {
 .vs__selected {
   position: absolute;
   z-index: 2;
+  white-space: nowrap;
+  max-width: 216px;
+  text-overflow:ellipsis;
+  overflow: hidden;
+  
+}
+
+a.button{
+  padding: 0.75rem 0.5rem;
+  width: 100%;
+}
+
+@media screen and (max-width: 750px){
+  .vs__selected {
+    max-width: 400px;
+  }
+
+  .v-select{
+    margin-bottom: 1rem;
+  }
+
+  a.button{
+    width:140px;
+  }
 }
 
 .clear-search-btn {
